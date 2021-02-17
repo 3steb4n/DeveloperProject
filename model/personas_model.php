cb2bc8133608d29474547baa8a030cb12a8e2e80
3steb4n <juangomez1947@outlook.com>
2021-02-17 16:38:19 -0500

    Se agrega archivo modelo

---
+++ model/personas_model.php
@@ -0,0 +1,27 @@
+<?php
+class VerificarUsuarios{
+    private $db;
+    private $datosUsuario;
+    private $usuarioExiste;
+
+    public function __contruct(){
+        $this->datosUsuario = array();
+    }
+
+    public function ConsultarUsuario($email, $clave){
+        require_once("../db/db.php");
+        $instanciaClase = new Conectar();
+        $instanciaMetodo = $instanciaClase->Conexion();
+        if($queryConsultaUsuarios = mysqli_query($instanciaMetodo, "SELECT * FROM USUARIOS WHERE EMAIL='$email' and CLAVE='$clave'")){
+            if(mysqli_num_rows($queryConsultaUsuarios) > 0){
+                while($userData = $queryConsultaUsuarios->fetch_assoc()){
+                    $this->datosUsuarios[] = $userData;
+                }
+                return $this->datosUsuarios;
+            }else{
+                return "El usuario está incorrecto o no existe.";
+            }
+        }
+    }
+}
+?>
