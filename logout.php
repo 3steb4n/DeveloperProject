fa764bdb49439492faded6a62afe05f2f05904cc
3steb4n <juangomez1947@outlook.com>
2021-05-19 12:47:56 -0500

    Se cambia mensaje al cerrar sesión

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@
 	$ldate=date( 'd-m-Y h:i:s A', time () );
 	mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE userEmail = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
 	session_unset();
-	$_SESSION['errmsg']="¡Regrese pronto! Ud ha cerrado sesión!";
+	$_SESSION['errmsg']="Sesión cerrada";
 	?>
 	<script language="javascript">
 	document.location="index.php";
