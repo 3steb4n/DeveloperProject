9197fee1afd0ed4e3b23014ceedb95ee18a99b7b
3steb4n <juangomez1947@outlook.com>
2021-05-19 15:30:13 -0500

    Se quita footer

--- pending-orders.php
+++ pending-orders.php
@@ -176,7 +176,6 @@ while($row=mysqli_fetch_array($query))
 <?php echo include('includes/brands-slider.php');?>
 <!-- ============================================== BRANDS CAROUSEL : END ============================================== -->	</div><!-- /.container -->
 </div><!-- /.body-content -->
-<?php include('includes/footer.php');?>
 
 	<script src="assets/js/jquery-1.11.1.min.js"></script>
 	
